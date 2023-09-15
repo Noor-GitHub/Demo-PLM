b975e146090cc83ca3fb10d2b36a1b11dd90b160
Noorullah-GitHub <noorullahansarism@gmail.com>
2023-09-15 16:26:21 +0530

    Hi i am Noor
    i have added 1 Class - name is TestScript2

--- src/test/java/testDemo/TestScript1.java
+++ src/test/java/testDemo/TestScript1.java
@@ -5,6 +5,8 @@ public class TestScript1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("I am Demo Class");
+		
+		System.out.println("i made some changes here");
 	}
 
 }
