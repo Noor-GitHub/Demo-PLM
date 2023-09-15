24b3c163ccb10357a76d2d078daac236cd441498
Noor-GitHub <noor2movie@gmail.com>
2023-09-15 16:17:07 +0530

    This is My Demo class - 1st Commit

---
+++ src/test/java/testDemo/TestScript1.java
@@ -0,0 +1,10 @@
+package testDemo;
+
+public class TestScript1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I am Demo Class");
+	}
+
+}
